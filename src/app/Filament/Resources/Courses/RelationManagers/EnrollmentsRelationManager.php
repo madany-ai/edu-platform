88077b0062084ce6439a601122d1d97b1ba2052f
Madany <mohamedgamalmadany@gmail.com>
2026-08-15 02:19:20 +0300

    fix: incorrect Action namespace causing 500 error in EnrollmentsRelationManager

--- src/app/Filament/Resources/Courses/RelationManagers/EnrollmentsRelationManager.php
+++ src/app/Filament/Resources/Courses/RelationManagers/EnrollmentsRelationManager.php
@@ -10,7 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Notifications\Notification;
 
 use Filament\Schemas\Schema;
